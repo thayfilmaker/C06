8007efd29457af0d38b1b176f6ecc78e0cbfd2ed
thayfilmaker <thayssilvalog@gmail.com>
2025-02-10 20:21:22 +0000

    Add files via upload

---
+++ C06THAY/ex02/ft_rev_params.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thasousa <thasousa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 23:39:20 by thasousa          #+#    #+#             */
+/*   Updated: 2025/02/07 00:49:14 by thasousa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	r;
+
+	r = argc - 1 ;
+	while (r > 0)
+	{
+		ft_putstr(argv[r]);
+		write(1, "\n", 1);
+		r--;
+	}
+}
